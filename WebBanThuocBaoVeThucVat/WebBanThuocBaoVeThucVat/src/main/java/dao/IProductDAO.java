083a02bf6a07151e61677bbdd63dc1541a571d26
21130526LeThanhTam <21130526@st.hcmuaf.edu.vn>
2024-03-31 23:59:54 +0700

    login gg

---
+++ WebBanThuocBaoVeThucVat/WebBanThuocBaoVeThucVat/src/main/java/dao/IProductDAO.java
@@ -0,0 +1,28 @@
+package dao;
+
+import bean.Product;
+import bean.Product;
+import bean.Products;
+
+import java.util.List;
+
+public interface IProductDAO {
+    List<Products> findAll1();
+//    List<Products> findAll2();
+
+    List<Product> findAll2();
+
+    List<Products> findNewPro1();
+
+    List<Products> findNewPro2();
+
+    List<Products> findDiscountPro1();
+
+    List<Products> findDiscountPro2();
+
+    List<Products> findById(int id);
+
+    List<Products> findByPriceMax(String id);
+
+    List<Products> findByPriceMin(String id);
+}
