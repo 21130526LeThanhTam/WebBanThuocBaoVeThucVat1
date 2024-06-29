8cb0bb38a69375a642670f31a47fe27ec3b1445f
linh2113 <21130091@st.hcmuaf.edu.vn>
2024-06-29 16:10:37 +0700

    login with Twitter

---
+++ WebBanThuocBaoVeThucVat/src/main/java/controller/LoginGithubHandler.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/loginByGithub")
+public class LoginGithubHandler extends HttpServlet {
+
+
+
+}
