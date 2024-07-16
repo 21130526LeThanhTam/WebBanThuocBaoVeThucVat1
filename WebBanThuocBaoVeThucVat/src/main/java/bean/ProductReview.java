920a61b1c5482e82d424f62d5a3eaeecdba58f73
thaison <21130514@st.hcmuaf.edu.vn>
2024-07-16 10:22:46 +0700

    model and dao cmt

--- WebBanThuocBaoVeThucVat/src/main/java/bean/ProductReview.java
+++ WebBanThuocBaoVeThucVat/src/main/java/bean/ProductReview.java
@@ -13,6 +13,7 @@ public class ProductReview {
     int id;
     int id_product;
     int id_user;
+    String user_name; //chứa user_name của người dùng
     int rating; //rating từ 1-5
     String content;
     Date create_at;
