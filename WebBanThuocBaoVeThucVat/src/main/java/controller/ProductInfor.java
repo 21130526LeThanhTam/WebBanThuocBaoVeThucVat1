5c62fc0e3ba19b3f82b0c4ab1ac5d0e881201352
thaison <21130514@st.hcmuaf.edu.vn>
2024-07-17 10:05:45 +0700

    done danhgiasanpham trong trang chi tiet san pham

--- WebBanThuocBaoVeThucVat/src/main/java/controller/ProductInfor.java
+++ WebBanThuocBaoVeThucVat/src/main/java/controller/ProductInfor.java
@@ -6,9 +6,11 @@ package controller;
 import Service.IProductService;
 import Service.ProductService;
 import bean.Comment;
+import bean.ProductReview;
 import bean.Products;
 import bean.User;
 import dao.CommentDAO;
+import dao.ProductReviewDao;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,42 +24,63 @@ import java.util.List;
 @WebServlet(urlPatterns = "/ProductInfor")
 public class ProductInfor extends HttpServlet {
 
+    // Thêm phương thức xử lý AJAX
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String id_product = request.getParameter("id_product");
-        int id = Integer.parseInt(id_product);
-        if(id_product != null){
-            IProductService pro = new ProductService();
-            Products proID = pro.findById(id);
-            request.setAttribute("proID", proID);
+        int id_product = Integer.parseInt(request.getParameter("id_product"));
+        IProductService pro = new ProductService();
+        Products proID = pro.findById(id_product);
+        request.setAttribute("proID", proID);
+        ProductReviewDao dao = new ProductReviewDao();
+        List<ProductReview> productReviews;
+        productReviews = dao.getListReview(id_product);
+        double averageRating = averageRating(productReviews);
+        request.setAttribute("productReviews", productReviews);
+        request.setAttribute("averageRating", averageRating);
+        if (request.getHeader("X-Requested-With") == null) {
+            System.out.println("vào thông tin đơn hàng");
+            request.getRequestDispatcher("thong-tin-don-hang.jsp").forward(request, response);
+        } else {
+            System.out.println("vào comment.jsp");
+            request.getRequestDispatcher("productReviews.jsp").forward(request, response);
         }
-        CommentDAO dao = new CommentDAO();
-        List<Comment> comments = dao.getAllComments();
-        int count = comments.size();
-        request.setAttribute("comments", comments);
-        request.setAttribute("commentCount", count);
-
-        request.getRequestDispatcher("thong-tin-don-hang.jsp").forward(request,response);
     }
 
+    public double averageRating(List<ProductReview>productReviews){
+        double sum = 0;
+        for (ProductReview review : productReviews){
+            sum +=review.getRating();
+        }
+        return sum/ productReviews.size();
+    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        String commentText = request.getParameter("commentText");
-        HttpSession session = request.getSession();
-        User user = (User) session.getAttribute("user");
-
-        if(user != null){
-            Comment comment = new Comment();
-            comment.setUsername(username);
-            comment.setCommentText(commentText);
-            comment.setEmail(user.getEmail());
-
-            CommentDAO dao = new CommentDAO();
-            dao.addComment(comment);
-
-            response.sendRedirect(request.getContextPath() + "/ProductInfor");
-        }else{
-            response.sendRedirect(request.getContextPath() + "/login");
-        }
+        //chức năng cho người dùng đánh giá sản phẩm ở dạng comment
+//        request.setCharacterEncoding("UTF-8");
+//        response.setCharacterEncoding("UTF-8");
+//        String commentText = request.getParameter("commentText");
+//        int rating = Integer.parseInt(request.getParameter("rating"));
+//        HttpSession session = request.getSession();
+//        User user = (User) session.getAttribute("user");
+//        String id_product = request.getParameter("productId");
+//        System.out.println(id_product);
+//        int productId = Integer.parseInt(id_product);
+//
+//        if (user != null) {
+//            ProductReview review = new ProductReview();
+//            review.setUser_name(user.getUsername());
+//            review.setContent(commentText);
+//            review.setRating(rating);
+//            review.setId_user(user.getId());
+//            review.setId_product(productId);
+//
+//            ProductReviewDao dao = new ProductReviewDao();
+//            dao.insertProductReview(review);
+//
+//            response.sendRedirect(request.getContextPath() + "/ProductInfor?id_product=" + productId);
+//        } else {
+//            response.sendRedirect(request.getContextPath() + "/login");
+//        }
+        doGet(request,response);
     }
+
 }
--- WebBanThuocBaoVeThucVat/src/main/java/dao/ProductReviewDao.java
+++ WebBanThuocBaoVeThucVat/src/main/java/dao/ProductReviewDao.java
@@ -10,7 +10,10 @@ import java.util.stream.Collectors;
 public class ProductReviewDao {
     // Lấy ra danh sách các comment thuộc về sản phẩm nào đó.
     public List<ProductReview> getListReview(int productId) {
-        String sql = "SELECT id, id_product, id_user, user_name, rating, content, create_at FROM product_reviews WHERE id_product = ?";
+        String sql = "SELECT product_reviews.id, product_reviews.id_product, product_reviews.id_user, users.user_name as user_name,users.picture as picture, product_reviews.rating, product_reviews.content, product_reviews.create_at " +
+                "FROM product_reviews " +
+                "JOIN users ON product_reviews.id_user = users.id " +
+                "WHERE product_reviews.id_product = ?";
         List<ProductReview> productReviewList = JDBIConnector.getJdbi().withHandle(handle ->
                 handle.createQuery(sql)
                         .bind(0, productId)
@@ -20,7 +23,10 @@ public class ProductReviewDao {
     }
     // Lấy ra danh sách các comment có rating cụ thể
     public List<ProductReview> getListReviewByRating(int productId, int rating) {
-        String sql = "SELECT id, id_product, id_user, user_name, rating, content, create_at FROM product_reviews WHERE id_product = ? AND rating = ?";
+        String sql = "SELECT product_reviews.id, product_reviews.id_product, product_reviews.id_user, users.user_name as user_name,users.picture as picture, product_reviews.rating, product_reviews.content, product_reviews.create_at " +
+                "FROM product_reviews " +
+                "JOIN users ON product_reviews.id_user = users.id " +
+                "WHERE product_reviews.id_product = ? AND product_reviews.rating = ?";
         List<ProductReview> productReviewList = JDBIConnector.getJdbi().withHandle(handle ->
                 handle.createQuery(sql)
                         .bind(0, productId)
@@ -29,7 +35,6 @@ public class ProductReviewDao {
                         .collect(Collectors.toList()));
         return productReviewList;
     }
-
     public List<ProductReview> getListReviewByRating1(int productId) {
         return getListReviewByRating(productId, 1);
     }
@@ -48,5 +53,39 @@ public class ProductReviewDao {
 
     public List<ProductReview> getListReviewByRating5(int productId) {
         return getListReviewByRating(productId, 5);
+    }
+    public void insertProductReview(ProductReview review) {
+        String sql = "INSERT INTO product_reviews (id_product, id_user, rating, content) VALUES (?, ?, ?, ?)";
+        JDBIConnector.getJdbi().useHandle(handle ->
+                handle.createUpdate(sql)
+                        .bind(0, review.getId_product())
+                        .bind(1, review.getId_user())
+                        .bind(2, review.getRating())
+                        .bind(3, review.getContent())
+                        .execute()
+        );
+    }
+    // Lấy ra danh sách các phản hồi cho một bình luận cụ thể
+    public List<ProductReview> getReplies(int reviewId) {
+        String sql = "SELECT product_reviews.id, product_reviews.id_product, product_reviews.id_user, users.user_name as user_name,users.picture as picture, product_reviews.rating, product_reviews.content, product_reviews.create_at, product_reviews.parent_id " +
+                "FROM product_reviews " +
+                "JOIN users ON product_reviews.id_user = users.id " +
+                "WHERE product_reviews.parent_id = ?";
+        return JDBIConnector.getJdbi().withHandle(handle ->
+                handle.createQuery(sql)
+                        .bind(0, reviewId)
+                        .mapToBean(ProductReview.class)
+                        .collect(Collectors.toList()));
+    }
+    public static void main(String[] args) {
+        ProductReviewDao dao = new ProductReviewDao();
+//        ProductReview review = new ProductReview();
+//        review.setId_product(4);
+//        review.setId_user(3);
+//        review.setRating(4);
+//        review.setContent("Sản phẩm ngon, sẽ ủng hộ");
+//        System.out.println(dao.getListReviewByRating(3,5));
+//       dao.insertProductReview(review);
+//        System.out.println(dao.getListReview(3));
     }
 }
